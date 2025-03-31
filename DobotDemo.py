0e0e6b2ef6090ed92eeddc8b85d9d632a24cbc06
Dobot <88758937+Dobot-Arm@users.noreply.github.com>
2025-03-31 14:30:29 +0800

    Update DobotDemo.py

--- DobotDemo.py
+++ DobotDemo.py
@@ -2,7 +2,6 @@ from dobot_api import DobotApiFeedBack,DobotApiDashboard
 import threading
 from time import sleep
 import re
-import sys
 
 class DobotDemo:
     def __init__(self, ip):
@@ -11,11 +10,15 @@ class DobotDemo:
         self.feedPortFour = 30004
         self.dashboard = None
         self.feedInfo = []
+        self.__globalLockValue = threading.Lock()
         
         class item:
             def __init__(self):
-                self.robotMode = 0     #
+                self.robotMode = -1     #
                 self.robotCurrentCommandID = 0
+                self.MessageSize = -1
+                self.DigitalInputs =-1
+                self.DigitalOutputs = -1
                 # 自定义添加所需反馈数据
 
         self.feedData = item()  # 定义结构对象
@@ -30,43 +33,51 @@ class DobotDemo:
         print("使能成功")
 
         # 启动状态反馈线程
-        threading.Thread(target=self.GetFeed, daemon=True).start()
+        feed_thread = threading.Thread(
+            target=self.GetFeed)  # 机器状态反馈线程
+        feed_thread.daemon = True
+        feed_thread.start()
 
         # 定义两个目标点
-        point_a = [0, 0, 202, -4, 9, -164]
-        point_b = [-10, 0, 202, -4, 9, -164]
+        point_a = [146.3759,-283.4321,332.3956,177.7879,-1.8540,147.5821]
+        point_b = [146.3759,-283.4321,432.3956,177.7879,-1.8540,147.5821]
 
         # 走点循环
         while True:
-            #self.RunPoint(point_a)
-            #self.RunPoint(point_b)
-            sleep(10000)
+            print("DI:", self.feedData.DigitalInputs,"2DI:", bin(self.feedData.DigitalInputs),"--16:",hex(self.feedData.DigitalInputs))
+            print("DO:", self.feedData.DigitalOutputs,"2DO:" ,bin(self.feedData.DigitalOutputs),"--16:",hex(self.feedData.DigitalOutputs))
+            print("robomode",self.feedData.robotMode)
+            sleep(2)
 
     def GetFeed(self):
         # 获取机器人状态
         while True:
             feedInfo = self.feedFour.feedBackData()
-            if feedInfo != None:   
-                if hex((feedInfo['test_value'][0])) == '0x123456789abcdef':
-                    self.feedData.robotMode = feedInfo['robot_mode'][0]
-                    self.feedData.robotCurrentCommandID = feedInfo['currentcommandid'][0]
-                    # 自主添加所需机械臂反馈的数据
-                    '''
-                    self.feedData.robotErrorState = feedInfo['error_status'][0]
-                    self.feedData.robotEnableStatus = feedInfo['enable_status'][0]
-                    self.feedData.robotCurrentCommandID = feedInfo['currentcommandid'][0]
-                    '''
+            with self.__globalLockValue:
+                if feedInfo is not None:   
+                    if hex((feedInfo['TestValue'][0])) == '0x123456789abcdef':
+                        # 基础字段
+                        self.feedData.MessageSize = feedInfo['len'][0]
+                        self.feedData.robotMode = feedInfo['RobotMode'][0]
+                        self.feedData.DigitalInputs = feedInfo['DigitalInputs'][0]
+                        self.feedData.DigitalOutputs = feedInfo['DigitalOutputs'][0]
+                        # 自定义添加所需反馈数据
+                        '''
+                        self.feedData.DigitalOutputs = int(feedInfo['DigitalOutputs'][0])
+                        self.feedData.RobotMode = int(feedInfo['RobotMode'][0])
+                        self.feedData.TimeStamp = int(feedInfo['TimeStamp'][0])
+                        '''
 
     def RunPoint(self, point_list):
         # 走点指令
-        recvmovemess = self.dashboard.MovJ(*point_list, 1)
+        recvmovemess = self.dashboard.MovJ(*point_list, 0)
         print("MovJ:", recvmovemess)
         print(self.parseResultId(recvmovemess))
         currentCommandID = self.parseResultId(recvmovemess)[1]
         print("指令 ID:", currentCommandID)
         #sleep(0.02)
         while True:  #完成判断循环
-            
+
             print(self.feedData.robotMode)
             if self.feedData.robotMode == 5 and self.feedData.robotCurrentCommandID == currentCommandID:
                 print("运动结束")
